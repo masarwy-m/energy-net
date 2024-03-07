608d350602ef47b38d00f61872632259f9ee34b9
sarah-keren <sarah.e.keren@gmail.com>
2024-03-07 12:23:17 +0200

    temp

---
+++ energy_net/dynamics/storage_dynamics.py
@@ -0,0 +1,11 @@
+from energy_net.dynamics.energy_dynamcis import StorageDynamics, ProductionDynamics
+
+
+class PVDynamics(ProductionDynamics):
+    def __init__(self, dc_capacity, net_connection_size, efficiency, capacity):
+        pass
+
+
+class StorageUnitDynamics(StorageDynamics):
+    def __init__(self, dc_capacity, net_connection_size, efficiency, capacity):
+        pass
