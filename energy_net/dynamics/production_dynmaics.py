c83bf02a57dc33b34ea7ffebc949af3c9c5f6d90
Itay Segev <itaysegev11@gmail.com>
2024-03-23 18:43:19 +0200

    Add basic Battery dynamic and update dynamics and entities

---
+++ energy_net/dynamics/production_dynmaics.py
@@ -0,0 +1,15 @@
+from energy_net.dynamics.energy_dynamcis import  ProductionDynamics
+
+
+class PVDynamics(ProductionDynamics):
+    def __init__(self, dc_capacity, net_connection_size, efficiency, capacity):
+        pass
+
+    def do(self, action, params):
+        pass
+
+    def predict(self, action, params, state):
+        pass
+
+
+
--- energy_net/dynamics/storage_dynamics.py
+++ energy_net/dynamics/storage_dynamics.py
@@ -1,11 +1,43 @@
-from energy_net.dynamics.energy_dynamcis import StorageDynamics, ProductionDynamics
-
-
-class PVDynamics(ProductionDynamics):
-    def __init__(self, dc_capacity, net_connection_size, efficiency, capacity):
-        pass
-
+from dynamics.energy_dynamcis import StorageDynamics
+from defs import EnergyAction, State
 
 class StorageUnitDynamics(StorageDynamics):
     def __init__(self, dc_capacity, net_connection_size, efficiency, capacity):
         pass
+
+    def do(self, action, params):
+        pass
+
+    def predict(self, action, params, state):
+        pass
+
+    def get_current_discharge_capability(self):
+        pass
+
+    def predict_discharge_capability(self, state):
+        pass
+
+    def get_current_charge_capability(self):
+        pass
+
+    def predict_charge_capability(self, state): 
+        pass       
+
+
+
+class BatteryDynamics(StorageDynamics):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def do(self, action:EnergyAction, params:dict):
+        if action == 'charge':
+            curr_device = params['device']
+            new_cpacity =  curr_device.capacity + params['charge_amount']
+            curr_device.capacity = new_cpacity
+        else:
+            raise ValueError('Invalid action')
+
+    def predict(self, action, params, state):
+        pass
+
+
