ab51015127fdd1ac8b996adcc279423e27485e23
Itay Segev <itaysegev11@gmail.com>
2024-03-25 19:18:15 +0200

    Add new types and constants

--- energy_net/defs.py
+++ energy_net/defs.py
@@ -1,7 +1,6 @@
-from typing import Callable, Any, TypedDict
+from typing import Callable, Any, TypedDict, Optional
 
-EnergyAction = dict[str, Any]
-State = dict[str, Any]
+# EnergyAction = dict[str, Any]
 Reward = dict[str, float]
 
 AmountPricePair = tuple[float, float]
@@ -11,6 +10,33 @@ ProductionFn = Callable[[Any, ...], AmountPricePair]
 
 Bid = [float, float]
 
+
+class EnergyAction(TypedDict, total=False):
+    produce: Optional[float]
+    consume: Optional[float]
+    charge: Optional[float]
+
+##################### States ######################
+State = dict[str, Any]
+
 class BatteryState(TypedDict):
     capacity: float
     state_of_charge: float
+
+class PVState(TypedDict):
+    nominal_power: float
+    efficiency: float
+
+
+class HeaterState(TypedDict):
+    efficiency: float
+    max_electric_power : float
+
+
+
+##################### Constants ######################
+INITAL_CAPACITY = 0.0
+MAX_ELECTRIC_POWER = 100
+NO_EFFICIENCY = 1
+MIN_POWER = 0
+
