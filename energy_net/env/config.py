cdedad1768567905e70a797c962b873b36a236c6
Itay Segev <itaysegev11@gmail.com>
2024-04-03 17:52:05 +0300

    Add pettingzoo api

---
+++ energy_net/env/config.py
@@ -0,0 +1,13 @@
+
+#################### CONSTANTS ####################
+NO_EFFICIENCY = 1.0
+MAX_EFFICIENCY = 1.0
+MIN_EFFICIENCY = 0.0
+NO_CHARGE = 0.0
+MIN_CHARGE = 0.0 
+MIN_CAPACITY = 0.0
+MAX_CAPACITY = float('inf')
+MIN_POWER = 0.0
+MAX_ELECTRIC_POWER = float('inf')
+DEFAULT_TIME_STEP = 1_000
+###################################################
