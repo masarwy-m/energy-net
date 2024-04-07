50077ff141356e74a08b52d9ba4a32f3a6f78c19
Itay Segev <itaysegev11@gmail.com>
2024-04-07 18:58:46 +0300

    Fix states types

--- energy_net/defs.py
+++ energy_net/defs.py
@@ -37,94 +37,92 @@ class EnergyAction(TypedDict, total=False):
 State = dict[str, Any]
 
 class BatteryState(np.ndarray):
-    def __new__(cls, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, lifetime_constant, current_time):
-        obj = np.asarray([[energy_capacity], [power_capacity], [state_of_charge], [charging_efficiency], [discharging_efficiency], [lifetime_constant], [current_time]], dtype=float).view(cls)
+    def __new__(cls, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, current_time):
+        obj = np.array([energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, current_time], dtype=np.float32).view(cls)
         return obj
 
-    def __init__(self, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, lifetime_constant, current_time):
+    def __init__(self, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, current_time):
         self.energy_capacity = energy_capacity
         self.power_capacity = power_capacity
         self.state_of_charge = state_of_charge
         self.charging_efficiency = charging_efficiency
         self.discharging_efficiency = discharging_efficiency
-        self.lifetime_constant = lifetime_constant
         self.current_time = current_time
 
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        self.current_time = obj[6, 0]
-        self.lifetime_constant = obj[5, 0]
-        self.discharging_efficiency = obj[4, 0]
-        self.charging_efficiency = obj[3, 0]
-        self.state_of_charge = obj[2, 0]
-        self.power_capacity = obj[1, 0]
-        self.energy_capacity = obj[0, 0]
+        self.current_time = obj[5]
+        self.discharging_efficiency = obj[4]
+        self.charging_efficiency = obj[3]
+        self.state_of_charge = obj[2]
+        self.power_capacity = obj[1]
+        self.energy_capacity = obj[0]
         
     @property
     def current_time(self):
-        return self[6, 0]
+        return self[5]
 
     @current_time.setter
     def current_time(self, value):
-        self[6, 0] = value
-        
-    @property
-    def lifetime_constant(self):
-        return self[5, 0]
-
-    @lifetime_constant.setter
-    def lifetime_constant(self, value):
-        self[5, 0] = value
+        if self.shape == ():
+            return
+        self[5] = value
         
     @property
     def discharging_efficiency(self):
-        return self[4, 0]
+        return self[4]
 
     @discharging_efficiency.setter
     def discharging_efficiency(self, value):
-        self[4, 0] = value
+        if self.shape == ():
+            return
+        self[4] = value
         
     @property
     def charging_efficiency(self):
-        return self[3, 0]
+        return self[3]
 
     @charging_efficiency.setter
     def charging_efficiency(self, value):
-        self[3, 0] = value
+        if self.shape == ():
+            return
+        self[3] = value
         
     @property
     def state_of_charge(self):
-        return self[2, 0]
+        return self[2]
 
     @state_of_charge.setter
     def state_of_charge(self, value):
-        self[2, 0] = value
+        if self.shape == ():
+            return
+        self[2] = value
 
     @property
     def power_capacity(self):
-        return self[1, 0]
+        return self[1]
 
     @power_capacity.setter
     def power_capacity(self, value):
-        self[1, 0] = value
+        if self.shape == ():
+            return
+        self[1] = value
 
     @property
     def energy_capacity(self):
-        return self[0, 0]
+        return self[0]
 
     @energy_capacity.setter
     def energy_capacity(self, value):
-        self[0, 0] = value
-
-# class ProducerState(TypedDict):
-#     max_produce: float
-#     genrated_power: float
+        if self.shape == ():
+            return
+        self[0] = value
 
 
 class ProducerState(np.ndarray):
     def __new__(cls, max_produce, production):
-        obj = np.asarray([[production], [max_produce]], dtype=float).view(cls)
+        obj = np.array([production, max_produce], dtype=np.float32).view(cls)
         return obj
 
     def __init__(self, max_produce, production=MIN_PRODUCTION):
@@ -134,34 +132,33 @@ class ProducerState(np.ndarray):
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        self.max_produce = obj[1, 0]
-        self.production = obj[0, 0]
+        self.max_produce = obj[1]
+        self.production = obj[0]
 
     @property
     def max_produce(self):
-        return self[1, 0]
+        return self[1]
 
     @max_produce.setter
     def max_produce(self, value):
-        self[1, 0] = value
+        if self.shape == ():
+            return
+        self[1] = value
 
     @property
     def production(self):
-        return self[0, 0]
+        return self[0]
 
     @production.setter
     def production(self, value):
-        self[0, 0] = value
+        if self.shape == ():
+            return
+        self[0] = value
 
-
-
-# class ConsumerState(TypedDict):
-#     efficiency: float
-#     max_electric_power : float
 
 class ConsumerState(np.ndarray):
     def __new__(cls, max_electric_power, efficiency, consumption=NO_CONSUMPTION):
-        obj = np.asarray([[consumption], [max_electric_power], [efficiency]], dtype=float).view(cls)
+        obj = np.array([consumption, max_electric_power, efficiency], dtype=np.float32).view(cls)
         return obj
 
     def __init__(self, max_electric_power, efficiency, consumption=NO_CONSUMPTION):
@@ -172,33 +169,39 @@ class ConsumerState(np.ndarray):
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        self.max_electric_power = obj[1, 0]
-        self.efficiency = obj[2, 0]
-        self.consumption = obj[0, 0]
+        self.max_electric_power = obj[1]
+        self.efficiency = obj[2]
+        self.consumption = obj[0]
 
     @property
     def max_electric_power(self):
-        return self[1, 0]
+        return self[1]
 
     @max_electric_power.setter
     def max_electric_power(self, value):
-        self[1, 0] = value
+        if self.shape == ():
+            return
+        self[1] = value
 
     @property
     def efficiency(self):
-        return self[2, 0]
+        return self[2]
 
     @efficiency.setter
     def efficiency(self, value):
-        self[2, 0] = value
+        if self.shape == ():
+            return
+        self[2] = value
 
     @property
     def consumption(self):
-        return self[0, 0]
+        return self[0]
 
     @consumption.setter
     def consumption(self, value):
-        self[0, 0] = value
+        if self.shape == ():
+            return
+        self[0] = value
 
 
 
