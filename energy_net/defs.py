258e352116f08752392c570855e5a233de611c01
ElinorG11 <elinor.g12@gmail.com>
2024-04-07 14:02:02 +0300

    update storage entity and dynamics to support numpy array

--- energy_net/defs.py
+++ energy_net/defs.py
@@ -36,9 +36,86 @@ class EnergyAction(TypedDict, total=False):
 ##################### States ######################
 State = dict[str, Any]
 
-class BatteryState(TypedDict):
-    capacity: float
-    state_of_charge: float
+class BatteryState(np.ndarray):
+    def __new__(cls, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, lifetime_constant, current_time):
+        obj = np.asarray([[energy_capacity], [power_capacity], [state_of_charge], [charging_efficiency], [discharging_efficiency], [lifetime_constant], [current_time]], dtype=float).view(cls)
+        return obj
+
+    def __init__(self, energy_capacity, power_capacity, state_of_charge, charging_efficiency, discharging_efficiency, lifetime_constant, current_time):
+        self.energy_capacity = energy_capacity
+        self.power_capacity = power_capacity
+        self.state_of_charge = state_of_charge
+        self.charging_efficiency = charging_efficiency
+        self.discharging_efficiency = discharging_efficiency
+        self.lifetime_constant = lifetime_constant
+        self.current_time = current_time
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.current_time = obj[6, 0]
+        self.lifetime_constant = obj[5, 0]
+        self.discharging_efficiency = obj[4, 0]
+        self.charging_efficiency = obj[3, 0]
+        self.state_of_charge = obj[2, 0]
+        self.power_capacity = obj[1, 0]
+        self.energy_capacity = obj[0, 0]
+        
+    @property
+    def current_time(self):
+        return self[6, 0]
+
+    @current_time.setter
+    def current_time(self, value):
+        self[6, 0] = value
+        
+    @property
+    def lifetime_constant(self):
+        return self[5, 0]
+
+    @lifetime_constant.setter
+    def lifetime_constant(self, value):
+        self[5, 0] = value
+        
+    @property
+    def discharging_efficiency(self):
+        return self[4, 0]
+
+    @discharging_efficiency.setter
+    def discharging_efficiency(self, value):
+        self[4, 0] = value
+        
+    @property
+    def charging_efficiency(self):
+        return self[3, 0]
+
+    @charging_efficiency.setter
+    def charging_efficiency(self, value):
+        self[3, 0] = value
+        
+    @property
+    def state_of_charge(self):
+        return self[2, 0]
+
+    @state_of_charge.setter
+    def state_of_charge(self, value):
+        self[2, 0] = value
+
+    @property
+    def power_capacity(self):
+        return self[1, 0]
+
+    @power_capacity.setter
+    def power_capacity(self, value):
+        self[1, 0] = value
+
+    @property
+    def energy_capacity(self):
+        return self[0, 0]
+
+    @energy_capacity.setter
+    def energy_capacity(self, value):
+        self[0, 0] = value
 
 # class ProducerState(TypedDict):
 #     max_produce: float
--- energy_net/utils/utils.py
+++ energy_net/utils/utils.py
@@ -1,3 +1,6 @@
+from scipy.integrate import quad
+from scipy.misc import derivative
+
 from typing import Callable, Any, TypedDict
 
 from defs import State
@@ -33,3 +36,27 @@ def get_value_by_type(dict, wanted_type):
             return value
     
     return None
+
+def unit_conversion(dest_units: str, x: float, T: tuple[float, float]) -> float:
+    """
+    Function for unit conversion. Calculate energy by integrating the power function
+    over the specified time interval. Calculate energy by derivating the energy function.
+
+    Parameters:
+        dest_units : Indicates the direction of the conversion.
+        x : function
+            May be the power function as a function of time or the energy function as function of time.
+        T : tuple
+            A tuple representing the time interval (start, end).
+
+    Returns:
+        float
+            The calculated energy or power.
+    """
+    if dest_units == 'W':
+        # Differentiate the energy function over the time interval
+        y = derivative(x, T, dx=1e-6, n=1)
+    elif dest_units == 'J':
+        # Integrate the power function over the time interval
+        y, _ = quad(x, T[0], T[1])
+    return y
