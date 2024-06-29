ba16eed1f82eea8964777d31754340b4ab4089f1
masarwy-m <90391513+masarwy-m@users.noreply.github.com>
2024-06-29 23:14:51 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,31 @@
+# Configuration file for Sphinx documentation builder.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+
+project = 'Energy-Net'
+author = 'Your Name'
+copyright = 'Copyright © Your Company'
+version = '1.0'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    # Add more extensions as needed
+]
+
+# The name of the main (index) file, without the file extension.
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
 
